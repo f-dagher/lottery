dba669d97cc375c912a314cd5723f42fd23ce916
f-dagher <fdagher95@gmail.com>
2020-06-29 17:23:00 -0400

    Updates

---
+++ precache-manifest.d85972279e30927f8e83a6b20d6ef965.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df1340a60c2984db509469f2f27948c",
+    "url": "/lottery/index.html"
+  },
+  {
+    "revision": "d0dc3ffefaa72e1422f5",
+    "url": "/lottery/static/css/main.67a59c52.chunk.css"
+  },
+  {
+    "revision": "3929d641c0beb4133332",
+    "url": "/lottery/static/js/2.55c82abb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lottery/static/js/2.55c82abb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0dc3ffefaa72e1422f5",
+    "url": "/lottery/static/js/main.130f4bb4.chunk.js"
+  },
+  {
+    "revision": "51b69ae4496104546da9",
+    "url": "/lottery/static/js/runtime-main.07511d75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lottery/static/media/logo.5d5d9eef.svg"
+  }
+]);
